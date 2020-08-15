c90e68068d9b689112fb204cbff4bd5dadff3034
Mohamed Al Ameen <mdalameen.s@gmail.com>
2020-08-16 01:52:18 +0530

    Basic home page with bottom menu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageType _currentPage = PageType.values[0];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +17,43 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Text('data'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _buildBottomItems(),
+        currentIndex: _currentPage.index,
+        onTap: _onPageSelected,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.yellow,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+      ),
     );
   }
+
+  _onPageSelected(int index) {
+    _currentPage = PageType.values[index];
+    setState(() {});
+  }
+
+  List<BottomNavigationBarItem> _buildBottomItems() {
+    var icons = <IconData>[
+      Icons.trending_up,
+      Icons.category,
+      Icons.shopping_cart,
+      Icons.more_horiz
+    ];
+    var labels = <String>['Ranking', 'Categories', 'Cart', 'More'];
+    return List.generate(
+        4,
+        (index) => BottomNavigationBarItem(
+            icon: Icon(icons[index],
+                color:
+                    index == _currentPage.index ? Colors.black : Colors.grey),
+            title: Text(
+              labels[index],
+              style: TextStyle(color: Colors.black),
+            )));
+  }
 }
+
+enum PageType { ranking, category, cart, more }
